6d6e660e6c08433c950e06c53ec4aaad0b3e2b3a
Andrei Sarandan <80057863+AndreiSarandan@users.noreply.github.com>
2022-10-01 21:49:48 +0300

    Update bitcoin_price_index_cs50.py

--- bitcoin_price_index_cs50.py
+++ bitcoin_price_index_cs50.py
@@ -5,12 +5,14 @@ try:
     # extract bitcoin price value
     r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     json = r.json()
-    z = (json['bpi']['USD']['rate']).replace(',', '')
-    mynum = float(sys.argv[1])
+    z = (json['bpi']['USD']['rate']).replace(',', '')#price
+    mynum = float(sys.argv[1])#formatting
     x = (float(z)*mynum)
     print("$", end="")
     y = f"{x:,.4f}"
     print(y, end="")
+    
+#error handling    
 except IndexError:
     sys.exit('Missing command-line argument ')
 except ValueError:
